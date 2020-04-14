6cd3acbd7d8f108d7eb00a416c66d10aa7d4b185
LoboArkano <pipiolo_mananes@hotmail.com>
2020-04-14 15:54:36 -0500

    Complete my_select method

--- main.rb
+++ main.rb
@@ -13,8 +13,18 @@ module Enumerable
     index = 0
 
     while index < size
-      yield self[index], item
+      yield self[item], index
       index += 1
+      item += 1
     end
+  end
+
+  def my_select
+    arr = []
+
+    my_each do |item|
+      arr.push(item) if yield item
+    end
+    arr
   end
 end
